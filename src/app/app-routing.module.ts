2b3dfd1ca8944c63dc9cbee12f57e4fab6e3410f
Alex Gualli <alex.gualli.ga@gmail.com>
2019-05-17 13:04:51 -0500

    pantalla principal HOME

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' }
-
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  //{ path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+  { path: 'setting', loadChildren: './pages/setting/setting.module#SettingPageModule' },
+  {
+    path: 'home', loadChildren: './pages/home/home.module#HomePageModule'
+  }
 ];
 
 @NgModule({
